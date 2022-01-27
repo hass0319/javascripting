6ddae74366f0e03f6acfe62d5d1b57a3f346cb1c
Amro Hassan <79529406+hass0319@users.noreply.github.com>
2022-01-26 22:57:42 -0500

    Delete vscode  directory

--- vscode /whichschool.js
+++
@@ -1,8 +0,0 @@
-function whichSchool(age){
-  if (age >=0 && age <13){
-    return 'elementary school'
-  }else if(age >=13 && age <=18){
-    return 'secondary school';
-  }else return 'lighthouse Labs';
-}
-console.log(whichSchool(14));
